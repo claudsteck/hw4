77ea31ab12785c5acf53c8d075a3ec1ae308cc94
Claudia Steck <claudia.steck@kellogg.northwestern.edu>
2024-03-09 23:09:05 +0000

    commit

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,7 +1,7 @@
 class EntriesController < ApplicationController
 
   def new
-    @user = User.find_by({ "id" => session["user_id"] })
+    @current_user = User.find_by({"id" => session["user_id"]})
   end
 
   def create
